c6bec78242b8510470810231a571f93dadc4aa4d
Mahesh Jaganiya <jagzmz@gmail.com>
2020-01-15 18:00:19 +0530

    Optimized Valdation Logic

--- validation.js
+++ validation.js
@@ -1,21 +1,37 @@
 "use strict";
 var _ = require("lodash");
-module.exports = (Model, options) => {
-  let customValidation = ["StringValidation","NumberValidation"]
+var debug = require("debug")("loopback:component-validator:validation");
+var beforeSaveValidators = [];
+var customValidators = [];
+
+module.exports = app => {
+  let customValidation = ["StringValidation", "NumberValidation"];
 
   _.forEach(customValidation, prop => {
     if (typeof this[prop] === "function") {
-      this[prop](Model, options);
+      this[prop](app);
     } else {
       throw new Error(`Function defination for '${prop}' not given`);
     }
   });
+  attachBeforeSave(app);
 };
 
-this.StringValidation = (Model, options) => {
-  require("./lib/stringValidation")(Model, options);
+this.StringValidation = app => {
+  var stringValidator = require("./lib/stringValidation");
+  customValidators.push(stringValidator.setup(app));
+};
+this.NumberValidation = app => {
+  var numberValidator = require("./lib/numberValidation");
+  customValidators.push(numberValidator.setup(app));
 };
 
-this.NumberValidation = (Model, options) => {
-  require("./lib/numberValidation")(Model, options);
-};
+function attachBeforeSave(app) {
+  _.keys(app.models).forEach(Model => {
+    app.models[Model].observe("before save", async ctx => {
+      for (let validator of customValidators) {
+        validator[Model].validate(ctx);
+      }
+    });
+  });
+}
